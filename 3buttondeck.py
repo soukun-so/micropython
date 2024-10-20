626d8d600d0c92c70939ecbcfe706392ef4c8109
sasaso <sido0903@gmail.com>
2024-10-20 22:41:59 +0900

    Update 3buttondeck.py
    light up led

--- 3buttondeck.py
+++ 3buttondeck.py
@@ -7,7 +7,10 @@ import time
 
 keyboard = Keyboard(usb_hid.devices)
 
-# ピン番号を指定して、GPIOの設定
+led1 = digitalio.DigitalInOut(board.GP0)
+led1.direction = digitalio.Direction.OUTPUT
+led1.value = False
+
 led = digitalio.DigitalInOut(board.GP14)
 led.direction = digitalio.Direction.OUTPUT
 led.value = True
@@ -16,7 +19,6 @@ button = digitalio.DigitalInOut(board.GP15)
 button.direction = digitalio.Direction.INPUT
 button.pull = digitalio.Pull.DOWN
 
-# ピン番号を指定して、GPIOの設定
 led2 = digitalio.DigitalInOut(board.GP26)
 led2.direction = digitalio.Direction.OUTPUT
 led2.value = True
@@ -25,7 +27,6 @@ button2 = digitalio.DigitalInOut(board.GP27)
 button2.direction = digitalio.Direction.INPUT
 button2.pull = digitalio.Pull.DOWN
 
-# ピン番号を指定して、GPIOの設定
 led3 = digitalio.DigitalInOut(board.GP21)
 led3.direction = digitalio.Direction.OUTPUT
 led3.value = True
@@ -39,24 +40,30 @@ while True:
     print(button.value)
     
     if button.value :
-        keyboard.press(Keycode.WINDOWS, Keycode.D)  # Windowsキー+Dを送信
-        keyboard.release_all()  # キーを離す
+        led1.value = True
+        keyboard.press(Keycode.WINDOWS, Keycode.D)
+        keyboard.release_all()
         time.sleep(3)
+        led1.value = False
         
     if button2.value :
-        keyboard.press(Keycode.WINDOWS, Keycode.TAB)  # Windowsキー+TABを送信
-        keyboard.release_all()  # キーを離す
+        led1.value = True
+        keyboard.press(Keycode.WINDOWS, Keycode.TAB)
+        keyboard.release_all()
         time.sleep(3)
+        led1.value = False
         
     if button3.value :
-        keyboard.press(Keycode.U)  # Uを送信
-        keyboard.release_all()  # キーを離す
-        keyboard.press(Keycode.N)  # Nを送信
-        keyboard.release_all()  # キーを離す
-        keyboard.press(Keycode.K)  # Kを送信
-        keyboard.release_all()  # キーを離す
-        keyboard.press(Keycode.O)  # O
-        keyboard.release_all()  # キーを離す
+        led1.value = True
+        keyboard.press(Keycode.U) 
+        keyboard.release_all() 
+        keyboard.press(Keycode.N)
+        keyboard.release_all() 
+        keyboard.press(Keycode.K) 
+        keyboard.release_all()
+        keyboard.press(Keycode.O)
+        keyboard.release_all() 
         time.sleep(3)
+        led1.value = False
         
     time.sleep(0.3)     # 1秒待機
